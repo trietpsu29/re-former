11de83712d877800dd94fc22ca7ce8f06ae6e31d
Triet Psu <112594221+trietpsu29@users.noreply.github.com>
2026-08-19 22:38:00 +0700

    Add edit user controllers and routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,17 @@ class UsersController < ApplicationController
       render :new, status: :unprocessable_entity
     end
   end
+  def edit
+    @user = User.find(params[:id])
+  end
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to edit_user_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
 
   private
     def user_params
--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  resources :users, only: [ :new, :create ]
+  resources :users, only: [ :new, :create, :edit, :update ]
 end
