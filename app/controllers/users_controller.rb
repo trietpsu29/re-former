5dbfea959e3df973ac9f806580a345e6d58bf169
Triet Psu <112594221+trietpsu29@users.noreply.github.com>
2026-08-19 21:22:56 +0700

    Add user create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,17 @@
 class UsersController < ApplicationController
   def new
   end
-
   def create
+    # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
+  private
+    def user_params
+      params.expect(user: [ :username, :email, :password ])
+    end
 end
